1b3797f7d3df969a86c297d301e28241973a84d4
Michael Chase <3686226+reallistic@users.noreply.github.com>
2017-12-05 08:56:39 -0800

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='bitty',
 
-    version='0.0.2',
+    version='0.0.3',
 
     description='Collect Crypto info',
     long_description=long_description,
